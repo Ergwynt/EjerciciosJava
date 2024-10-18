bccbeaba81812266b466442fde719134d4e6b4b9
Ergwynt <aday18005@gmail.com>
2024-10-18 15:18:26 +0100

    Cambios

--- 17/script.js
+++ 17/script.js
@@ -6,20 +6,19 @@ misiones.forEach(mision => {
         
         // Mostrar un mensaje al completar la misión
         if (this.classList.contains('completada')) {
-            mostrarMensaje(`¡Misión completada: ${this.textContent}!`);
+            mostrarMensaje(`¡Misión completada: ${this.textContent}!`, 'success');
         } else {
-            mostrarMensaje(`Misión marcada como incompleta: ${this.textContent}.`);
+            mostrarMensaje(`Misión marcada como incompleta: ${this.textContent}.`, 'error');
         }
     });
 });
 
-function mostrarMensaje(mensaje) {
+function mostrarMensaje(mensaje, tipo) {
     const mensajeElemento = document.createElement('div');
     mensajeElemento.textContent = mensaje;
-    mensajeElemento.classList.add('mensaje');
+    mensajeElemento.classList.add('mensaje', tipo); // Añadir la clase tipo
     document.body.appendChild(mensajeElemento);
     
-
     setTimeout(() => {
         mensajeElemento.remove();
     }, 3000);
