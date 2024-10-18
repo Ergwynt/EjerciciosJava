208a7814b401d6da9e002b724860284884d63cb5
Ergwynt <aday18005@gmail.com>
2024-10-18 15:47:12 +0100

    Arreglo cosas

--- 18/script.js
+++ 18/script.js
@@ -13,6 +13,7 @@ class Villano {
 }
 
 const joker = new Villano("Destruir Gotham");
+const fallido = document.getElementById("error");
 
 const planText = document.getElementById("plan-text");
 planText.textContent = joker.obtenerPlan();
@@ -21,12 +22,16 @@ const botonCambiarPlan = document.getElementById("cambiar-plan");
 const inputNuevoPlan = document.getElementById("nuevo-plan");
 
 botonCambiarPlan.addEventListener('click', () => {
+    
     const nuevoPlan = inputNuevoPlan.value;
     if (nuevoPlan) {
+        fallido.style.visibility = "hidden";
         joker.cambiarPlan(nuevoPlan);
         planText.textContent = joker.obtenerPlan();
         inputNuevoPlan.value = "";
     } else {
-        alert("Por favor, introduce un plan.");
+        fallido.style.visibility = "visible";
+        fallido.style.backgroundColor = "red";
     }
 });
+
